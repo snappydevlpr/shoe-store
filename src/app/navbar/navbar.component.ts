d4bfb96e8fe8ea7cf9cf3254d5f83e9fa330b986
Austin Suarez <austinmsuarez@gmail.com>
2021-08-08 12:31:50 -0500

    created responsive menu on expand/minimize window

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, from, fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,43 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // Checks if screen size is less than 1024 pixels
+    const checkScreenSize = () => {
+      if(document.body.offsetWidth <= 425){
+        document.getElementById("dropdown-menu")?.style.setProperty("display","none");
+      }
+      else{
+        document.getElementById("mobile-menu")?.style.setProperty("display","none");
+
+      }
+      console.log(document.body.offsetWidth)
+    };
+
+    // Create observable from window resize event
+    const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(100),map(checkScreenSize));
+    //creating a subscription to the resize 
+    screenSizeChanged$.subscribe()
   }
 
+  dropdownMenuToggle(){
+    if(document.getElementById("dropdown-menu")?.style.display == "block"){
+      document.getElementById("dropdown-menu")?.style.setProperty("display","none");
+
+    }
+    else{
+      document.getElementById("dropdown-menu")?.style.setProperty("display","block");
+
+    }
+  }
+
+  dropdownMenuMobileToggle(){
+    if(document.getElementById("mobile-menu")?.style.display == "block"){
+      document.getElementById("mobile-menu")?.style.setProperty("display","none");
+
+    }
+    else{
+      document.getElementById("mobile-menu")?.style.setProperty("display","block");
+
+    }
+  }
 }
